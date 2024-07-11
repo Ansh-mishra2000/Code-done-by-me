e51ac0f4851fa25580d4dc69063870825f3afc21
Ansh-mishra2000 <161422695+Ansh-mishra2000@users.noreply.github.com>
2024-07-11 14:23:50 +0530

    Update searchInARowWiseAndColumnWiseSortedMatrix.cpp

--- searchInARowWiseAndColumnWiseSortedMatrix.cpp
+++ searchInARowWiseAndColumnWiseSortedMatrix.cpp
@@ -1,23 +1,36 @@
+//apporach I
+#include <bits/stdc++.h> 
+pair<int, int> search(vector<vector<int>> matrix, int x)
+{   
+    pair<int,int>p;
+    for(int i=0;i<matrix.size();i++){
+        for(int j=0;j<matrix[i].size();j++){
+            if(matrix[i][j]==x){
+                p=make_pair(i,j);
+                return p;
+            }
+        }
+    }
+    return make_pair(-1,-1);
+}
+//approach II
 #include <bits/stdc++.h> 
 pair<int, int> search(vector<vector<int>> matrix, int x)
 {
-    // Write your code here.
-    pair <int,int> p;
-    p.first=-1;
-    p.second=-1;
-    int low=0;
-    int n=matrix.size();
-    int high=n*n-1;
-    while(low<=high ){
-        int mid=(low+high)/2;
-        if(x==matrix[mid/n][mid%n]){
-            p.first=mid/n;
-            p.second=mid%n;
-            return p;
-        }
-        else if(x>matrix[mid/n][mid%n]) low=mid+1;
-        else high=mid-1;
+    int row=matrix.size();
+    int col=matrix[0].size();
+
+    int rowIndex=0;
+    int colIndex=col-1;
+
+    while(rowIndex<row && colIndex>=0){
+        int element=matrix[rowIndex][colIndex];
+
+        if(element==x) return make_pair(rowIndex,colIndex);
+
+        if(element<x) rowIndex++;
+
+        else colIndex--;
     }
-    return p;
-    
+    return make_pair(-1,-1);
 }
